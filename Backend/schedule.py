b1a4c9af524d18fff4ee254ed62317fbeb30db53
Harsh Shelar <30754252+TeraSurror@users.noreply.github.com>
2020-03-22 21:47:55 +0530

    Sorting Bug Fixed

--- Backend/schedule.py
+++ Backend/schedule.py
@@ -14,7 +14,6 @@ def generateSchedule(tasks,deadline):
 
     daysForEachTask = int(deadline/len(tasks))
 
-    ## Doubt
     tasks = sorted(tasks, key = lambda task : task.weight, reverse=True)
     
 
@@ -23,12 +22,12 @@ def generateSchedule(tasks,deadline):
     end = daysForEachTask-1
     for i in range(0,deadline):
         if i>=start and i<=end:
-            schedule.append(Schedule(i+1, tasks[numTask].name))
+            schedule.append(Schedule(i+1, tasks[numTask]))
         else:
             numTask += 1
             start = end
             end += daysForEachTask
-            schedule.append(Schedule(i+1, tasks[numTask].name))
+            schedule.append(Schedule(i+1, tasks[numTask]))
     
     return schedule
         
@@ -38,16 +37,22 @@ numTasks = int(input("Number of tasks : "))
 deadline = int(input("Deadline : "))
 tasks = []
 
-for i in range(0,numTasks):
-    name = input("Enter Name of task : ")
-    weight = input("Enter weight of the task (from 1 to 10) : ")
-    task = Task(name, weight)
-    tasks.append(task)
 
-schedule = generateSchedule(tasks,deadline)
+if numTasks>deadline:
+    print("Not possible")
+    print("Pray to God🙏🙏🙏")
 
-for s in schedule:
-    print("Day", s.day, ":", s.task, sep=' ')
+else : 
+    for i in range(0,numTasks):
+        name = input("Enter Name of task : ")
+        weight = input("Enter weight of the task (from 1 to 10) : ")
+        task = Task(name, weight)
+        tasks.append(task)
+
+    schedule = generateSchedule(tasks,deadline)
+
+    for s in schedule:
+        print("Day", s.day, ":", s.task.name, sep=' ')
 
 
 
